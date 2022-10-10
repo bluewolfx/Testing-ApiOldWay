19ab51684b8f4025a07b032c3591be0a83a63f5c
ricardobanon <ricardobanon@gmail.com>
2022-10-10 03:44:03 +0100

    Completed Device Servlet with doGet and doPost
    Started implementing on ResApiClient - main class with menu for the client to insert info or get info, geDeviceData method, and setDeviceData Method (incomplete)

--- src/DeviceServlet.java
+++ src/DeviceServlet.java
@@ -9,6 +9,42 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 
-interface DeviceRepository extends JpaRepository<Device, Long>{
+public class DeviceRepository extends HttpServlet{
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServeletResponse response) throws IOException, ServletException {
+        
+		String requestUrl = request.getRequestURI();
+		String name = requestUrl.substring("/device/".length());
+		
+		Device device = DataStore.getInstance().getDevice(id);
+		
+		if(device != null){
+			String json = "{\n";
+			json += "\"name\": " + JSONObject.quote(device.getName()) + ",\n";
+			json += "\"brand\": " + JSONObject.quote(device.getBrand()) + ",\n";
+			json += "\"creationTime\": " + device.getCreationTime() + "\n";
+            json += "\"id\": " + device.getId() + "\n";
+			json += "}";
+			response.getOutputStream().println(json);
+		}
+		else{
+			
+			response.getOutputStream().println("{}");
+		}
+	}
+	
+	
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		String name = request.getParameter("name");
+		String about = request.getParameter("brand");
+		Date creationTime = request.getParameter("creationTime");
+        Long id = request.getParameter("id");
+		
+		DataStore.getInstance().putDevice(new Device(name, brand, creationTime, id));
+    }
 
 }
--- src/RestApiClient.java
+++ src/RestApiClient.java
@@ -1,5 +1,61 @@
 package com.demo.demo;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.sql.Date;
+import java.util.Scanner;
+import org.json.JSONObject;
+
 public class RestApiClient {
+
+    public static void main(String... args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to device line editor");
+        System.out.println("Want to insert device or get device info?");
+        System.out.println("To insert type 'set' to get info type 'get'");
+        String getOrSet = scanner.nextLine();
+        if("get".equalsIgnoreCase(getOrSet)){
+            System.out.println();
+        }
+    }
+
+    public static string getDeviceData(Long id) throws IOException{
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:8080/device/" + id).openConnection();
+		
+		connection.setRequestMethod("GET");
+
+		int responseCode = connection.getResponseCode();
+		if(responseCode == 200){
+			String response = "";
+			Scanner sc = new Scanner(connection.getInputStream());
+			while(sc.hasNextLine()){
+				response += sc.nextLine();
+				response += "\n";
+			}
+			sc.close();
+
+			return response;
+		}
+		
+		// an error happened
+		return null;
+    }
+
+    public static void setDeviceData(String name, String brand, Date creationTime, Long id) throws IOException{
+		HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:8080/device/" + id).openConnection();
+
+        connection.setrequestMethod("POST");
+
+        String insertData = "name=" + URLEncoder.encode(name);
+        insertData += "brand=" + URLEncoder.encode(brand);
+        insertData += "creationTime=" + URLEncoder.encode(creationTime.toGMTString());
+        insertData += "id=" + URLEncoder.encode(id.toString());
+
+        connection.setDoOutput(true);
+
     
 }
